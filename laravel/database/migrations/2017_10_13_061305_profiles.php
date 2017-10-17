93b51a53c44c1ed88ce8aafd64f8eb78cbbc76b2
addictionWhite <enigma961@gmail.com>
2017-10-17 14:37:03 +0900

    add migration file

---
+++ laravel/database/migrations/2017_10_13_061305_profiles.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Profiles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('content', 255)->nullable();;
+            $table->binary('raw_image')->nullable();;
+            $table->binary('thumbnail_image')->nullable();;
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Profiles');
+    }
+}
